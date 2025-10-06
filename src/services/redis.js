c119eee29999892885e6d8d4b924c1a1f57ea6ee
Shailesh2003-arch <thoratshailesh220@gmail.com>
2025-10-06 14:05:47 +0530

    chore(redis-service): establish connection to local redis-server

---
+++ src/services/redis.js
@@ -0,0 +1,21 @@
+import { createClient } from "redis";
+
+// We use a client to interact with the redis server...
+// Also remember that, whenever there exists a server - there exists a client.
+// Why we need client ?...
+// Cuz at the end of the day, redis is a server. So to interact with a server we obviously needs a client...
+
+const redisClient = createClient(
+    {
+        url:"redis://localhost:6379"
+    }
+)
+
+
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+
+await redisClient.connect();
+
+
+// Now this is the instance of redis connection that you established with your redis-server...
+export default redisClient;
